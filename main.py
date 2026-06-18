c44943a3925c1144d31fd8286d215c23afb7323a
Vladislav Slobodchikov <vslobodchikov@vvo.local>
2026-06-19 00:31:53 +1000

    Add and update book data

--- main.py
+++ main.py
@@ -4,10 +4,33 @@ def book_list_view(library: dict) -> None:
         return
 
     print("Названия книг:")
-    index = 0
-    for book in library:
-        index += 1
-        print(f"{index}. {book}")
+    for i, book in enumerate(library, 1):
+        print(f"{i}. {book}")
+    print()
+
+
+def validate_book_data(title: str, author: str, year: int):
+    return False if type(title) != str or type(author) != str or type(year) != int else True
+
+
+def add_book(title: str, author: str, year: int) -> None:
+    if not validate_book_data(title, author, year):
+        print(f"Для добавления книги использованы некорректные данные.\n")
+        return
+
+    global books
+    if title in books:
+        input_data = input("Книга уже добавлена. Хотите обновить данные? (Да/Нет): ").lower().strip()
+        if input_data == "да":
+            books[title]["author"], books[title]["year"] = author, year
+            print(f"Данные по книге \"{title}\" обновлены.\n")
+        elif input_data == "нет":
+            print(f"Данные по книге \"{title}\" остались без изменений.\n")
+        else:
+            print("Введено некорректное значение. Данные не обновлены.\n")
+    else:
+        books[title] = {"author": author, "year": year, "availability": None}
+        print(f"Книга \"{title}\" добавлена в библиотеку.\n")
 
 
 books = {
@@ -16,4 +39,8 @@ books = {
             "availability": "В наличии"}
 }
 
+book_list_view(books)
+add_book("Кэрри", "Стивен Кинг", 1974)
+book_list_view(books)
+add_book("Кэрри", "Стивен Кинг", 2001)
 book_list_view(books)
